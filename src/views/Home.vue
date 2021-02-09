558747178c11888aa330a768e294c410c0171a80
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2021-02-09 16:39:02 +0000

    Added Routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae reiciendis commodi fugit vel voluptas expedita, distinctio sed ab iusto? Nulla perferendis ducimus deleniti perspiciatis repellat, quisquam id beatae labore sequi excepturi quia iste impedit dolorum atque unde quam. Delectus quibusdam ratione assumenda asperiores exercitationem odio rerum vero, nemo, sint laboriosam consectetur blanditiis tempore iure dolor fugiat? Voluptatibus quidem, sit velit maiores officiis totam hic commodi eos dolor sapiente error architecto laborum neque tempore soluta consectetur laboriosam, deleniti harum temporibus, suscipit eveniet placeat? Possimus minima consequatur expedita quidem a voluptatum voluptates nam nisi totam. In culpa voluptate, ea aliquam soluta iure!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae reiciendis commodi fugit vel voluptas expedita, distinctio sed ab iusto? Nulla perferendis ducimus deleniti perspiciatis repellat, quisquam id beatae labore sequi excepturi quia iste impedit dolorum atque unde quam. Delectus quibusdam ratione assumenda asperiores exercitationem odio rerum vero, nemo, sint laboriosam consectetur blanditiis tempore iure dolor fugiat? Voluptatibus quidem, sit velit maiores officiis totam hic commodi eos dolor sapiente error architecto laborum neque tempore soluta consectetur laboriosam, deleniti harum temporibus, suscipit eveniet placeat? Possimus minima consequatur expedita quidem a voluptatum voluptates nam nisi totam. In culpa voluptate, ea aliquam soluta iure!</p>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
---
+++ src/views/Jobs.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id"></div>
+    <h2> {{ job.title }} </h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                { title: 'UX Designer', id: 1, details: 'lorem' },
+                { title: 'Web Developer', id: 1, details: 'lorem' },
+                { title: 'Vue Developer', id: 1, details: 'lorem' },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
