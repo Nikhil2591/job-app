8e0294bcaadb9e798e1e27278df1498046708efd
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2021-02-09 17:10:14 +0000

    Redirects and 404 page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
-import Jobs from '../views/Jobs.vue'
+import NotFound from '../views/NotFound.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -16,8 +18,25 @@ const routes = [
   },
   {
     path: '/jobs',
-    name: 'jobs',
+    name: 'Jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
